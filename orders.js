7d8698eff6b925828109188aa7b6bc152b0c91a6
Jonathan <jmuller@dial-once.com>
2016-02-11 20:04:28 +0100

    Add orders helper

---
+++ orders.js
@@ -0,0 +1,28 @@
+/*
+ * var e = require('./orders');
+ */
+var orders = [];
+var products = require('./products');
+
+module.exports = {
+  add: function(x, y) {
+    orders.push({ x: x, y: y, products: {}});
+  },
+  addProduct: function(id, productId, count) {
+    orders[id].products[productId] = (orders[id].products[productId] || 0) + count;
+  },
+  deliver: function(id, productId, count) {
+    orders[id].products[productId] -= count;
+  },
+  get: function(id) {
+    return orders[id];
+  },
+  getWeight: function(id) {
+    var pKeys = Object.keys(orders[id].products);
+    var weight = 0;
+    for(var i = 0; i < pKeys.length; i++) {
+      weight += products.getWeight(pKeys[i]);
+    }
+    return weight;
+  }
+};
