701c88d6774f6f20d49b20956e12bb49e8c0d813
Gaelle Gomez <pussy@pukogames.com>
2016-02-11 21:18:49 +0100

    Use of services

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // H A S H   C O D E   2 0 1 6
 
 var lineReader = require('line-reader');
+var warehouseService = require('./warehouse');
+var productService = require('./products');
+var orderService = require('./orders');
 
 main(process.argv[2], process.argv[3]);
 
@@ -28,6 +31,7 @@ function main(inputFile, outputFile) {
     } else if (i === 2) {
       // products weigh
       split.forEach(function (product, index) {
+        productService.add(index, product);
         data.products[index] = product;
       });
     } else if (warehouseNb < 0) {
@@ -37,6 +41,7 @@ function main(inputFile, outputFile) {
     } else if (i < 3 + warehouseNb * 2 + 1) {
       // warehouses
       if (split.length == 2) {
+        warehouseService.add(Object.keys(data.warehouses).length, split[0], split[1]);
         data.warehouses[Object.keys(data.warehouses).length] = {
           x: split[0],
           y: split[1]
@@ -44,6 +49,7 @@ function main(inputFile, outputFile) {
       } else if (split.length === Object.keys(data.products).length) {
         data.warehouses[Object.keys(data.warehouses).length - 1].products = {};
         split.forEach(function (count, index) {
+          warehouseService.addProduct(Object.keys(data.warehouses).length - 1, index, count);
           data.warehouses[Object.keys(data.warehouses).length - 1].products[index] = count;
         });
       } else {
@@ -58,6 +64,7 @@ function main(inputFile, outputFile) {
       // orders
       if ((i - ordersNbIndex) % 3 === 1) {
         // x y
+        orderService.add(split[0], split[1]);
         data.orders[Object.keys(data.orders).length] = {
           x: split[0],
           y: split[1],
@@ -73,6 +80,7 @@ function main(inputFile, outputFile) {
           if (typeof data.orders[Object.keys(data.orders).length - 1].products[productType] == 'undefined') {
             data.orders[Object.keys(data.orders).length - 1].products[productType] = 0;
           }
+          orderService.addProduct(Object.keys(data.orders).length - 1, productType, 1);
           data.orders[Object.keys(data.orders).length - 1].products[productType]++;
         });
       }
