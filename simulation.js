a78adb8cf64aba2eef6769ee7e3825b0272b10f6
Jonathan <jmuller@dial-once.com>
2016-02-11 20:28:35 +0100

    Simulation file update with global vars

--- simulation.js
+++ simulation.js
@@ -1,7 +1,11 @@
-var drones = require('./drones');
+var drones = require('./drones'),
+  warehouses = require('./warehouse'),
+  products = require('./products'),
+  orders = require('./orders');
 
-module.exports = function(rows, columns, warehouses, startingWarehouseId, dronesCount, dronesCapacity, turns) {
+module.exports = function(rows, columns, startingWarehouseId, dronesCount, dronesCapacity, turns) {
   for(var i = 0; i < dronesCount; i++) {
     drones.add(i, warehouses.get(startingWarehouseId), turns, dronesCapacity);
   }
+
 };
